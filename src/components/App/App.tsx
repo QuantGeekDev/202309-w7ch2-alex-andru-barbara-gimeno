1e7aafde383cafbfdf597f9bd0a2e222b371f292
Alex Andru <alex007d@gmail.com>
2023-10-24 23:13:54 +0200

    Add loop function for API Connection

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,23 +2,53 @@ import { type CharacterData } from "../../type";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import { useState, useEffect } from "react";
 
+const getCharacters = async (quantity: number) => {
+  const charactersList: CharacterData[] = [];
+  for (
+    let characterCount = 1;
+    characterCount < quantity + 1;
+    characterCount++
+  ) {
+    const characterUrl = `https://swapi.dev/api/people/${characterCount}`;
+    const response = await fetch(characterUrl);
+    const characterApi = (await response.json()) as CharacterData;
+    characterApi.id = characterCount;
+    characterApi.avatarUrl = `https://starwars-visualguide.com/assets/img/characters/${characterCount}.jpg`;
+    charactersList.push(characterApi);
+  }
+  return charactersList;
+};
+const luke: CharacterData = {
+  avatarUrl: "",
+  created: "",
+  height: "",
+  id: 0,
+  mass: "",
+  name: "",
+  url: "",
+};
+
 const App = (): React.ReactElement => {
-  const [character, setCharacters] = useState<CharacterData>();
+  const [characters, setCharacters] = useState<CharacterData[]>([luke]);
 
   useEffect(() => {
     const loadApi = async () => {
-      const apiUrl = "https://swapi.dev/api/people/1/";
-      const response = await fetch(apiUrl);
-      const charactersApi = (await response.json()) as CharacterData;
-      setCharacters(charactersApi);
+      const characters = await getCharacters(30);
+      setCharacters(characters);
     };
     loadApi();
-  }, [character]);
+  }, [characters]);
 
   return (
     <div className="app">
       <ul>
-        <CharacterCard character={character!} />
+        {characters.map((character) => {
+          return (
+            <li>
+              <CharacterCard character={character} />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
