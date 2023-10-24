339535e6678d2343983a6f30b14c3da10ec3c695
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-10-24 23:50:04 +0200

    Add css at app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,21 @@
+import CharacterCard from "../Character/Character";
+import "./App.css";
+
 const App = (): React.ReactElement => {
-  return <div className="app"></div>;
+  const luke = {
+    id: 1,
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    created: "2014-12-09T13:50:51.644000Z",
+    url: "https://starwars-visualguide.com/assets/img/characters/1.jpg",
+  };
+  return (
+    <div className="app">
+      <h1 className="main__title"> Star Wars Characters</h1>
+      <img className="logo" src="logo-star-wars.jpg" alt="" />
+      <CharacterCard character={luke} />
+    </div>
+  );
 };
 export default App;
